0af64b631ddc2160ec503369679f78c680310a35
Paul Statezny <paulstatezny@users.noreply.github.com>
2014-10-28 22:49:00 -0700

    Refs #1 - Handle keypresses.

--- application/ui/components/game-window.jsx
+++ application/ui/components/game-window.jsx
@@ -6,9 +6,76 @@ var Player = require('./player');
 var Block  = require('./block');
 var _      = require('underscore');
 
+var keys = {
+    LEFT  : 37,
+    RIGHT : 39,
+    SPACE : 32
+};
+
+var validKeys = [37, 39, 32];
+
+var direction = {
+    LEFT  : -1,
+    RIGHT : 1
+};
+
 module.exports = React.createClass({
 
     displayName : 'GameWindow',
+
+    componentWillMount : function()
+    {
+        document.onkeydown = this.onKeyDown;
+        document.onkeyup   = this.onKeyUp;
+    },
+
+    componentWillUnmount : function()
+    {
+        document.onkeydown = undefined;
+        document.onkeyup   = undefined;
+    },
+
+    onKeyDown : function(e)
+    {
+        e = e || window.event;
+
+        if (! _.contains(validKeys, e.keyCode)) {
+            return;
+        }
+
+        if (e.keyCode === keys.RIGHT && this.state.direction === direction.RIGHT) {
+            return;
+        }
+
+        if (e.keyCode === keys.LEFT && this.state.direction === direction.LEFT) {
+            return;
+        }
+
+        if (e.keyCode === keys.LEFT) {
+            this.setState({direction : direction.LEFT});
+        } else if (e.keyCode === keys.RIGHT) {
+            this.setState({direction : direction.RIGHT});
+        } else if (e.keyCode === keys.SPACE && this.state.playing === false) {
+            this.setState({playing : true});
+        } else if (e.keyCode === keys.SPACE && this.state.playing === true) {
+            this.setState({playing : false});
+        }
+    },
+
+    onKeyUp : function(e)
+    {
+        e = e || window.event;
+
+        if (! _.contains(validKeys, e.keyCode)) {
+            return;
+        }
+
+        if (e.keyCode === keys.LEFT && this.state.direction === direction.LEFT) {
+            this.setState({direction : null});
+        } else if (e.keyCode === keys.RIGHT && this.state.direction === direction.RIGHT) {
+            this.setState({direction : null});
+        }
+    },
 
     getInitialState : function()
     {
@@ -23,7 +90,10 @@ module.exports = React.createClass({
             blocks : this.generateBlocks(),
 
             // Whether we are currently playing
-            playing : false
+            playing : false,
+
+            // Current direction of player movement
+            direction : null
         };
     },
 
