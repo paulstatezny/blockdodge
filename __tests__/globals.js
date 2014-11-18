5a17a27ab8046cf0ca50035008b9014c040b7dfd
Paul Statezny <paulstatezny@users.noreply.github.com>
2014-11-17 23:15:07 -0700

    Refs #master - Delete test and CI files.

--- __tests__/globals.js
+++
@@ -1,2 +0,0 @@
-__BACKEND__     = 'blockdodge.vm';
-__ENVIRONMENT__ = 'development';
--- __tests__/setup.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-// Ensure that chai accepts should-style assertions and is integrated with sinon.
-var chai      = require('chai');
-var sinonChai = require('sinon-chai');
-
-chai.should();
-chai.use(sinonChai);
